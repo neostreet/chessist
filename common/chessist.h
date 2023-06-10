3e1cef026c6fcd55710835307942430623fcb3a6
neostreet <lloyd.aidan@gmail.com>
2023-06-10 18:32:58 -0400

    update chessist with gargantua logic

--- common/chessist.h
+++ common/chessist.h
@@ -13,19 +13,19 @@
 #define IDB_TOOLBAR            110
 
 #define IDM_SET_LAST_MOVE      200
+#define IDM_PRINT_BOARD        201
+#define IDM_PRINT_GAME         202
+#define IDM_PRINT_MOVES        203
 
-#define IDM_TOGGLE_ORIENTATION 201
-#define IDM_TOGGLE_BOARD_SIZE  202
+#define IDM_TOGGLE_ORIENTATION 204
+#define IDM_TOGGLE_BOARD_SIZE  205
 
-#define IDM_PREV_MOVE          204
-#define IDM_NEXT_MOVE          205
-#define IDM_START_OF_GAME      206
-#define IDM_END_OF_GAME        207
-#define IDM_FPRINT_GAME_BIN    208
-#define IDM_FPRINT_GAME        209
-#define IDM_FPRINT_BD          210
+#define IDM_PREV_MOVE          206
+#define IDM_NEXT_MOVE          207
+#define IDM_START_OF_GAME      208
+#define IDM_END_OF_GAME        209
 
-#define IDM_ABOUT              211
+#define IDM_ABOUT              210
 
 #define IDC_STATIC -1
 
