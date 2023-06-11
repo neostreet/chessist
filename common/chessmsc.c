8c0add74e08f34d5c1f04cf5930db068129d92ff
neostreet <lloyd.aidan@gmail.com>
2023-06-11 12:15:04 -0400

    update chessist with gargantua logic

--- common/chessmsc.c
+++ common/chessmsc.c
@@ -3,6 +3,18 @@
 #include "chess.glb"
 #include "chess.fun"
 #include "chess.mac"
+#include "bitfuns.h"
+
+static unsigned char initial_board[] = {
+  (unsigned char)0x23, (unsigned char)0x45, (unsigned char)0x64, (unsigned char)0x32,
+  (unsigned char)0x11, (unsigned char)0x11, (unsigned char)0x11, (unsigned char)0x11,
+  (unsigned char)0x00, (unsigned char)0x00, (unsigned char)0x00, (unsigned char)0x00,
+  (unsigned char)0x00, (unsigned char)0x00, (unsigned char)0x00, (unsigned char)0x00,
+  (unsigned char)0x00, (unsigned char)0x00, (unsigned char)0x00, (unsigned char)0x00,
+  (unsigned char)0x00, (unsigned char)0x00, (unsigned char)0x00, (unsigned char)0x00,
+  (unsigned char)0xff, (unsigned char)0xff, (unsigned char)0xff, (unsigned char)0xff,
+  (unsigned char)0xed, (unsigned char)0xcb, (unsigned char)0xac, (unsigned char)0xde
+};
 
 extern char piece_ids[]; /* "RNBQK" */
 extern char fmt_str[];
@@ -39,37 +51,14 @@ void print_bd(struct game *gamept)
   int n;
   int square;
 
-  for (m = 0; m < 8; m++) {
-    for (n = 0; n < 8; n++) {
-      square = get_piece2(gamept,7 - m,n);
+  for (m = 0; m < NUM_RANKS; m++) {
+    for (n = 0; n < NUM_FILES; n++) {
+      square = get_piece2(gamept,(NUM_RANKS - 1) - m,n);
       printf("%c ",format_square(square));
     }
 
     putchar(0x0a);
   }
-}
-
-void fprint_game_bin(struct game *gamept,char *filename)
-{
-  FILE *fptr;
-
-  if ((fptr = fopen(filename,"w")) == NULL)
-    return;
-
-  fprintf(fptr,fmt_str,gamept->title);
-
-  set_initial_board(gamept);
-
-  for (gamept->curr_move = 0;
-       gamept->curr_move <= gamept->num_moves;
-       gamept->curr_move++) {
-
-    fprintf_move(fptr,gamept);
-
-    update_board(gamept,FALSE);
-  }
-
-  fclose(fptr);
 }
 
 void fprint_game(struct game *gamept,char *filename)
@@ -91,10 +80,29 @@ void fprint_game(struct game *gamept,char *filename)
     sprintf_move(gamept,buf,20);
     fprintf(fptr,fmt_str,buf);
 
-    update_board(gamept,FALSE);
+    update_board(gamept,NULL,NULL);
   }
 
   fclose(fptr);
+}
+
+void fprint_game2(struct game *gamept,FILE *fptr)
+{
+  char buf[20];
+
+  fprintf(fptr,fmt_str,gamept->title);
+
+  set_initial_board(gamept);
+
+  for (gamept->curr_move = 0;
+       gamept->curr_move <= gamept->num_moves;
+       gamept->curr_move++) {
+
+    sprintf_move(gamept,buf,20);
+    fprintf(fptr,fmt_str,buf);
+
+    update_board(gamept,NULL,NULL);
+  }
 }
 
 void fprint_bd(struct game *gamept,char *filename)
@@ -107,9 +115,9 @@ void fprint_bd(struct game *gamept,char *filename)
   if ((fptr = fopen(filename,"w")) == NULL)
     return;
 
-  for (m = 0; m < 8; m++) {
-    for (n = 0; n < 8; n++) {
-      square = get_piece2(gamept,7 - m,n);
+  for (m = 0; m < NUM_RANKS; m++) {
+    for (n = 0; n < NUM_FILES; n++) {
+      square = get_piece2(gamept,(NUM_RANKS - 1) - m,n);
       fprintf(fptr,"%c ",format_square(square));
     }
 
@@ -117,4 +125,161 @@ void fprint_bd(struct game *gamept,char *filename)
   }
 
   fclose(fptr);
+}
+
+void fprint_bd2(struct game *gamept,FILE *fptr)
+{
+  int m;
+  int n;
+  int square;
+
+  for (m = 0; m < NUM_RANKS; m++) {
+    for (n = 0; n < NUM_FILES; n++) {
+      square = get_piece2(gamept,(NUM_RANKS - 1) - m,n);
+      fprintf(fptr,"%c ",format_square(square));
+    }
+
+    fputc(0x0a,fptr);
+  }
+}
+
+void fprint_moves(struct game *gamept,char *filename)
+{
+  int n;
+  FILE *fptr;
+
+  if ((fptr = fopen(filename,"w")) == NULL)
+    return;
+
+  for (n = 0; n < gamept->num_moves; n++) {
+    fprintf(fptr,"%d %d\n",gamept->moves[n].from,gamept->moves[n].to);
+  }
+
+  fclose(fptr);
+}
+
+void fprint_moves2(struct game *gamept,FILE *fptr)
+{
+  int n;
+
+  for (n = 0; n < gamept->num_moves; n++) {
+    fprintf(fptr,"%d %d\n",gamept->moves[n].from,gamept->moves[n].to);
+  }
+}
+
+void set_initial_board(struct game *gamept)
+{
+  int n;
+
+  for (n = 0; n < CHARS_IN_BOARD; n++)
+    gamept->board[n] = initial_board[n];
+}
+
+void update_board(struct game *gamept,int *invalid_squares,int *num_invalid_squares)
+{
+  bool bKingsideCastle;
+  bool bQueensideCastle;
+
+  if (invalid_squares) {
+    invalid_squares[(*num_invalid_squares)++] = gamept->moves[gamept->curr_move].from;
+    invalid_squares[(*num_invalid_squares)++] = gamept->moves[gamept->curr_move].to;
+  }
+
+  set_piece1(gamept,gamept->moves[gamept->curr_move].to,
+    get_piece1(gamept,gamept->moves[gamept->curr_move].from));
+
+  set_piece1(gamept,gamept->moves[gamept->curr_move].from,0);  /* vacate previous square */
+
+  bKingsideCastle = (gamept->moves[gamept->curr_move].special_move_info == SPECIAL_MOVE_KINGSIDE_CASTLE);
+  bQueensideCastle = (gamept->moves[gamept->curr_move].special_move_info == SPECIAL_MOVE_QUEENSIDE_CASTLE);
+
+  if (bKingsideCastle) {
+    if (!(gamept->curr_move % 2)) {
+      // it's White's move
+      set_piece1(gamept,5,
+        get_piece1(gamept,7));
+      set_piece1(gamept,7,0);
+
+      if (invalid_squares) {
+        invalid_squares[(*num_invalid_squares)++] = 5;
+        invalid_squares[(*num_invalid_squares)++] = 7;
+      }
+    }
+    else {
+      // it's Blacks's move
+      set_piece1(gamept,61,
+        get_piece1(gamept,63));
+      set_piece1(gamept,63,0);
+
+      if (invalid_squares) {
+        invalid_squares[(*num_invalid_squares)++] = 61;
+        invalid_squares[(*num_invalid_squares)++] = 63;
+      }
+    }
+  }
+  else if (bQueensideCastle) {
+    if (!(gamept->curr_move % 2)) {
+      // it's White's move
+      set_piece1(gamept,3,
+        get_piece1(gamept,0));
+      set_piece1(gamept,0,0);
+
+      if (invalid_squares) {
+        invalid_squares[(*num_invalid_squares)++] = 3;
+        invalid_squares[(*num_invalid_squares)++] = 0;
+      }
+    }
+    else {
+      // it's Blacks's move
+      set_piece1(gamept,59,
+        get_piece1(gamept,56));
+      set_piece1(gamept,56,0);
+
+      if (invalid_squares) {
+        invalid_squares[(*num_invalid_squares)++] = 59;
+        invalid_squares[(*num_invalid_squares)++] = 56;
+      }
+    }
+  }
+}
+
+int get_piece1(struct game *gamept,int board_offset)
+{
+  unsigned int bit_offset;
+  unsigned short piece;
+  int piece_int;
+
+  bit_offset = board_offset * BITS_PER_BOARD_SQUARE;
+
+  piece = get_bits(BITS_PER_BOARD_SQUARE,gamept->board,bit_offset);
+  piece_int = piece;
+
+  if (piece & 0x8)
+    piece_int |= 0xfffffff0;
+
+  return piece_int;
+}
+
+int get_piece2(struct game *gamept,int row,int column)
+{
+  int board_offset;
+
+  board_offset = row * NUM_RANKS + column;
+  return get_piece1(gamept,board_offset);
+}
+
+void set_piece1(struct game *gamept,int board_offset,int piece)
+{
+  unsigned int bit_offset;
+
+  bit_offset = board_offset * BITS_PER_BOARD_SQUARE;
+  set_bits(BITS_PER_BOARD_SQUARE,gamept->board,bit_offset,piece);
+}
+
+void set_piece2(struct game *gamept,int row,int column,int piece)
+{
+  int board_offset;
+
+  board_offset = row * NUM_RANKS + column;
+  set_piece1(gamept,board_offset,piece);
 }
