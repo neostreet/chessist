718e6816bcecdcdc60a2e41cb4fa4b20cbea192d
neostreet <lloyd.aidan@gmail.com>
2024-02-18 11:54:34 -0500

    change read_binary_game() and write_binary_game() to skip reading of the board

--- common/chessrd.c
+++ common/chessrd.c
@@ -61,7 +61,7 @@ int read_binary_game(char *filename,struct game *gamept)
   if ((fhndl = open(filename,O_RDONLY | O_BINARY)) == -1)
     return 1;
 
-  bytes_to_read = sizeof (struct game) - sizeof gamept->moves;
+  bytes_to_read = sizeof (struct game) - sizeof gamept->moves - sizeof gamept->board;
 
   bytes_read = read(fhndl,(char *)gamept,bytes_to_read);
 
@@ -83,6 +83,9 @@ int read_binary_game(char *filename,struct game *gamept)
 
   close(fhndl);
 
+  set_initial_board(gamept);
+  position_game(gamept,gamept->num_moves);
+
   return 0;
 }
 
@@ -96,7 +99,7 @@ int write_binary_game(char *filename,struct game *gamept)
       S_IREAD | S_IWRITE)) == -1)
     return 1;
 
-  bytes_to_write = sizeof (struct game) - sizeof gamept->moves;
+  bytes_to_write = sizeof (struct game) - sizeof gamept->moves - sizeof gamept->board;
 
   bytes_written = write(fhndl,(char *)gamept,bytes_to_write);
 
