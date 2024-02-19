355b38d2626c5396eeb32eb6715cfc779e78a98a
neostreet <lloyd.aidan@gmail.com>
2024-02-19 15:37:24 -0500

    synch print_game() and sprintf_move() with chess_utilities versions

--- common/chessdbg.c
+++ common/chessdbg.c
@@ -49,21 +49,43 @@ char get_decoded_piece(struct game *gamept)
   return decode_piece(piece,FALSE);
 }
 
+char get_from_file(struct game *gamept)
+{
+  return 'a' + FILE_OF(gamept->moves[gamept->curr_move].from);
+}
+
 void print_game(struct game *gamept)
 {
+  int n;
+  int save_len;
+
   printf(fmt_str,gamept->title);
 
   set_initial_board(gamept);
 
   for (gamept->curr_move = 0;
-       gamept->curr_move <= gamept->num_moves;
+       gamept->curr_move < gamept->num_moves;
        gamept->curr_move++) {
 
-    sprintf_move(gamept,buf,20);
-    printf(fmt_str,buf);
+    sprintf_move(gamept,buf,20,true);
+
+    if (gamept->curr_move % 2) {
+      for (n = save_len; n < 12; n++)
+        putchar(' ');
+    }
+    else
+      save_len = strlen(buf);
+
+    printf("%s",buf);
+
+    if (gamept->curr_move % 2)
+      putchar(0x0a);
 
     update_board(gamept,NULL,NULL);
   }
+
+  if (gamept->num_moves % 2)
+    putchar(0x0a);
 }
 
 void fprintf_move(FILE *fptr,struct game *gamept)
@@ -76,58 +98,73 @@ void fprintf_move(FILE *fptr,struct game *gamept)
     gamept->moves[gamept->curr_move].to);
 }
 
-void sprintf_move(struct game *gamept,char *buf,int buf_len)
+void sprintf_move(struct game *gamept,char *buf,int buf_len,bool bInline)
 {
-  int bWhite;
+  bool bWhite;
   int put_count;
   char decoded_piece;
-  int bDone;
+  bool bDone;
   int from;
   int from_file;
   int to;
   int to_file;
   int to_rank;
 
-  if (!gamept->curr_move) {
-    strcpy(buf,"Starting position");
+  bWhite = !((gamept->curr_move) % 2);
+
+  if (bWhite || !bInline) {
+    sprintf(buf,"%2d. ",((gamept->curr_move) / 2) + 1);
     put_count = strlen(buf);
-
-    for ( ; put_count < buf_len - 1; put_count++)
-      buf[put_count] = ' ';
-
-    buf[put_count] = 0;
-
-    return;
   }
+  else
+    put_count = 0;
 
-  bWhite = !((gamept->curr_move-1) % 2);
-
-  sprintf(buf,"%2d. ",((gamept->curr_move-1) / 2) + 1);
-  put_count = 4;
-
-  if (!bWhite) {
+  if (!bInline && !bWhite) {
     sprintf(&buf[put_count],"...  ");
     put_count += 5;
   }
 
-  decoded_piece = get_decoded_piece(gamept);
-  bDone = TRUE;
+  if (gamept->moves[gamept->curr_move].special_move_info & SPECIAL_MOVE_KINGSIDE_CASTLE) {
+    sprintf(&buf[put_count],"O-O");
+    put_count += 3;
+  }
+  else if (gamept->moves[gamept->curr_move].special_move_info & SPECIAL_MOVE_QUEENSIDE_CASTLE) {
+    sprintf(&buf[put_count],"O-O-O");
+    put_count += 5;
+  }
+  else {
+    decoded_piece = get_decoded_piece(gamept);
 
-  if (decoded_piece == 'P') {
-    buf[put_count++] = decoded_piece;
-    bDone = FALSE;
+    if (decoded_piece == 'P')
+      bDone = false;
+    else {
+      buf[put_count++] = decoded_piece;
+      bDone = false;
+    }
+
+    if (!bDone) {
+      if (gamept->moves[gamept->curr_move].special_move_info & SPECIAL_MOVE_CAPTURE) {
+        if (decoded_piece == 'P')
+          buf[put_count++] = get_from_file(gamept);
+
+        buf[put_count++] = 'x';
+      }
+
+      to = gamept->moves[gamept->curr_move].to;
+      to_file = FILE_OF(to);
+      to_rank = RANK_OF(to);
+      buf[put_count++] = 'a' + to_file;
+      buf[put_count++] = '1' + to_rank;
+    }
   }
 
-  if (!bDone) {
-    to = gamept->moves[gamept->curr_move-1].to;
-    to_file = FILE_OF(to);
-    to_rank = RANK_OF(to);
-    buf[put_count++] = 'a' + to_file;
-    buf[put_count++] = '1' + to_rank;
-  }
+  if (gamept->moves[gamept->curr_move].special_move_info & SPECIAL_MOVE_CHECK)
+    buf[put_count++] = '+';
 
-  for ( ; put_count < buf_len - 1; put_count++)
-    buf[put_count] = ' ';
+  if (!bInline) {
+    for ( ; put_count < buf_len - 1; put_count++)
+      buf[put_count] = ' ';
+  }
 
   buf[put_count] = 0;
 }
