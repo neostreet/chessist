30a9fc284bdc5e9297728182dc55dee54edb1540
neostreet <lloyd.aidan@gmail.com>
2024-05-07 16:58:17 -0400

    synch with chess_utilities logic

--- common/chessrd.c
+++ common/chessrd.c
@@ -14,8 +14,31 @@
 #include "chess.fun"
 #include "chess.glb"
 #include "chess.mac"
+#include "bitfuns.h"
 
-extern char piece_ids[]; /* "RNBQK" */
+static unsigned char initial_board[] = {
+  (unsigned char)0x23, (unsigned char)0x45, (unsigned char)0x64, (unsigned char)0x32,
+  (unsigned char)0x11, (unsigned char)0x11, (unsigned char)0x11, (unsigned char)0x11,
+  (unsigned char)0x00, (unsigned char)0x00, (unsigned char)0x00, (unsigned char)0x00,
+  (unsigned char)0x00, (unsigned char)0x00, (unsigned char)0x00, (unsigned char)0x00,
+  (unsigned char)0x00, (unsigned char)0x00, (unsigned char)0x00, (unsigned char)0x00,
+  (unsigned char)0x00, (unsigned char)0x00, (unsigned char)0x00, (unsigned char)0x00,
+  (unsigned char)0xff, (unsigned char)0xff, (unsigned char)0xff, (unsigned char)0xff,
+  (unsigned char)0xed, (unsigned char)0xcb, (unsigned char)0xac, (unsigned char)0xde
+};
+
+static int force_values[] = {
+  FORCE_VALUE_PAWN,
+  FORCE_VALUE_ROOK,
+  FORCE_VALUE_KNIGHT,
+  FORCE_VALUE_BISHOP,
+  FORCE_VALUE_QUEEN,
+  FORCE_VALUE_KING
+};
+
+static char corrupted_msg[] = "game corrupted\n";
+static char bad_castle[] = "bad castle";
+static char bad_pawn_move[] = "bad pawn move";
 
 static char *bad_piece_move[] = {
   "bad rook move",
@@ -24,6 +47,18 @@ static char *bad_piece_move[] = {
   "bad queen move",
   "bad king move"
 };
+
+static int fen2pos(char *line,int line_len,unsigned char *pos_buf,int *black_to_play);
+
+short force_value_of(short piece)
+{
+  if (piece < 0)
+    piece *= -1;
+
+  piece--;
+
+  return force_values[piece];
+}
 
 int line_number(char *word,int wordlen)
 {
@@ -34,11 +69,11 @@ int line_number(char *word,int wordlen)
       if ((word[n] == '.') && (n == wordlen - 1))
         break;
 
-      return FALSE;
+      return false;
     }
   }
 
-  return TRUE;
+  return true;
 }
 
 int get_piece_type_ix(int chara)
@@ -52,6 +87,166 @@ int get_piece_type_ix(int chara)
   return 0; /* should never happen */
 }
 
+void set_initial_board(struct game *gamept)
+{
+  int n;
+
+  for (n = 0; n < CHARS_IN_BOARD; n++)
+    gamept->board[n] = initial_board[n];
+}
+
+int read_game(char *filename,struct game *gamept,char *err_msg)
+{
+  FILE *fptr;
+  int chara;
+  int end_of_file;
+  int m;
+  int n;
+  char word[WORDLEN];
+  int wordlen;
+  bool bHaveFirstWord;
+  int direction;
+  int word_no;
+  int dbg;
+  int retval;
+  int got_error;
+  bool bCheck;
+  bool bMate;
+
+  gamept->curr_move = -1;
+
+  if ((fptr = fopen(filename,"r")) == NULL)
+    return 1;
+
+  fscanf(fptr,"%d",&gamept->orientation);  /* get board orientation */
+                                   /* 0 = standard, 1 = black on bottom */
+
+  end_of_file = get_word(fptr,word,WORDLEN,&wordlen,&bCheck,&bMate);
+  bHaveFirstWord = true;
+
+  set_initial_board(gamept);
+
+  end_of_file = 0;
+  gamept->curr_move = 0;
+
+  gamept->title[0] = 0;
+  word_no = 0;
+  got_error = 0;
+
+  for ( ; ; ) {
+    if (word_no || !bHaveFirstWord)
+      end_of_file = get_word(fptr,word,WORDLEN,&wordlen,&bCheck,&bMate);
+
+    if (end_of_file)
+      break;
+
+    word_no++;
+
+    if (gamept->curr_move == dbg_move)
+      dbg = 1;
+
+    /* ignore line numbers: */
+    if (line_number(word,wordlen))
+      continue;
+
+    if (!strncmp(word,"title",5)) {
+      m = 5;
+      n = 0;
+
+      for ( ; ; ) {
+        chara = word[m++];
+
+        if (!chara)
+          break;
+
+        if (n < 79)
+          if (chara == '\\')
+            gamept->title[n++] = ' ';
+          else
+            gamept->title[n++] = chara;
+      }
+
+      gamept->title[n] = 0;
+
+      continue;
+    }
+
+    if (gamept->curr_move & 0x1)
+      direction = -1;           /* black's move */
+    else
+      direction = 1;            /* white's move */
+
+    gamept->moves[gamept->curr_move].special_move_info = 0;
+
+    switch(word[0]) {
+      case 'O':
+      case '0':
+        retval = do_castle(gamept,direction,word,wordlen,&gamept->moves[gamept->curr_move]);
+
+        if (retval) {
+          /*printf(corrupted_msg);*/
+          strcpy(err_msg,bad_castle);
+
+          got_error = 1;
+        }
+
+        break;
+
+      case 'R':
+      case 'N':
+      case 'B':
+      case 'Q':
+      case 'K':
+        retval = do_piece_move(gamept,direction,word,wordlen,&gamept->moves[gamept->curr_move]);
+
+        if (retval) {
+          /*printf(corrupted_msg);*/
+          strcpy(err_msg,bad_piece_move[get_piece_type_ix(word[0])]);
+
+          got_error = 1;
+        }
+
+        break;
+
+      default:
+        retval = do_pawn_move(gamept,direction,word,wordlen,&gamept->moves[gamept->curr_move]);
+
+        if (retval) {
+          /*printf(corrupted_msg);*/
+          strcpy(err_msg,bad_pawn_move);
+
+          got_error = 1;
+        }
+
+        break;
+    }
+
+    if (got_error)
+      break;
+
+    if (bCheck) {
+      gamept->moves[gamept->curr_move].special_move_info |= SPECIAL_MOVE_CHECK;
+    }
+
+    if (bMate) {
+      gamept->moves[gamept->curr_move].special_move_info |= SPECIAL_MOVE_MATE;
+    }
+
+    update_board(gamept->board,gamept,NULL,NULL);
+
+    gamept->curr_move++;
+  }
+
+  gamept->num_moves = gamept->curr_move;
+
+  fclose(fptr);
+
+  if (got_error)
+    return 3;
+
+  return 0;
+}
+
 int read_binary_game(char *filename,struct game *gamept)
 {
   int fhndl;
@@ -61,7 +256,7 @@ int read_binary_game(char *filename,struct game *gamept)
   if ((fhndl = open(filename,O_RDONLY | O_BINARY)) == -1)
     return 1;
 
-  bytes_to_read = sizeof (struct game) - sizeof gamept->moves - sizeof gamept->board;
+  bytes_to_read = sizeof (struct game) - sizeof gamept->moves;
 
   bytes_read = read(fhndl,(char *)gamept,bytes_to_read);
 
@@ -72,19 +267,14 @@ int read_binary_game(char *filename,struct game *gamept)
 
   bytes_to_read = gamept->num_moves * sizeof (struct move);
 
-  if (bytes_to_read) {
-    bytes_read = read(fhndl,(char *)gamept->moves,bytes_to_read);
+  bytes_read = read(fhndl,(char *)gamept->moves,bytes_to_read);
 
-    if (bytes_read != bytes_to_read) {
-      close(fhndl);
-      return 3;
-    }
+  if (bytes_read != bytes_to_read) {
+    close(fhndl);
+    return 3;
   }
 
   close(fhndl);
-
-  set_initial_board(gamept);
-  position_game(gamept,gamept->curr_move);
 
   return 0;
 }
@@ -99,7 +289,7 @@ int write_binary_game(char *filename,struct game *gamept)
       S_IREAD | S_IWRITE)) == -1)
     return 1;
 
-  bytes_to_write = sizeof (struct game) - sizeof gamept->moves - sizeof gamept->board;
+  bytes_to_write = sizeof (struct game) - sizeof gamept->moves;
 
   bytes_written = write(fhndl,(char *)gamept,bytes_to_write);
 
@@ -122,20 +312,27 @@ int write_binary_game(char *filename,struct game *gamept)
   return 0;
 }
 
-int ignore_character(int chara)
+int ignore_character(int chara,bool *bCheck,bool *bMate)
 {
+  if ((chara == '+') || (chara == '#'))
+    *bCheck = true;
+
+  if (chara == '#')
+    *bMate = true;
+
   if ((chara == 0x0d) ||
     (chara == '(') ||
     (chara == ')') ||
     (chara == 'x') ||
     (chara == '=') ||
-    (chara == '+'))
-    return TRUE;
+    (chara == '+') ||
+    (chara == '#'))
+    return true;
 
-  return FALSE;
+  return false;
 }
 
-int get_word(FILE *fptr,char *word,int maxlen,int *wordlenpt)
+int get_word(FILE *fptr,char *word,int maxlen,int *wordlenpt,bool *bCheck,bool *bMate)
 {
   int chara;
   int started;
@@ -147,6 +344,8 @@ int get_word(FILE *fptr,char *word,int maxlen,int *wordlenpt)
   started = 0;
   comment = 0;
   end_of_file = 0;
+  *bCheck = false;
+  *bMate = false;
 
   for ( ; ; ) {
     chara = fgetc(fptr);
@@ -159,7 +358,7 @@ int get_word(FILE *fptr,char *word,int maxlen,int *wordlenpt)
     }
 
     // ignore carriage returns and other characters
-    if (ignore_character(chara))
+    if (ignore_character(chara,bCheck,bMate))
       continue;
 
     /* end of line ? */
@@ -202,6 +401,153 @@ int get_word(FILE *fptr,char *word,int maxlen,int *wordlenpt)
   return end_of_file;
 }
 
+static int update_board_calls;
+static int dbg_update_board_call;
+static int dbg;
+
+void update_board(unsigned char *board,struct game *gamept,int *invalid_squares,int *num_invalid_squares)
+{
+  bool bKingsideCastle;
+  bool bQueensideCastle;
+  bool bBlack;
+  int from_piece;
+  int to_piece;
+
+  update_board_calls++;
+
+  if (dbg_update_board_call == update_board_calls)
+    dbg = 0;
+
+  bBlack = (gamept->curr_move % 2);
+
+  from_piece = get_piece1(board,gamept->moves[gamept->curr_move].from);
+  to_piece = get_piece1(board,gamept->moves[gamept->curr_move].to);
+
+  if (from_piece * to_piece < 0)
+    gamept->moves[gamept->curr_move].special_move_info |= SPECIAL_MOVE_CAPTURE;
+
+  switch (gamept->moves[gamept->curr_move].special_move_info) {
+    case SPECIAL_MOVE_PROMOTION_QUEEN:
+      from_piece = (bBlack ? QUEEN_ID * -1 : QUEEN_ID);
+
+      break;
+    case SPECIAL_MOVE_PROMOTION_ROOK:
+      from_piece = (bBlack ? ROOK_ID * -1 : ROOK_ID);
+
+      break;
+    case SPECIAL_MOVE_PROMOTION_BISHOP:
+      from_piece = (bBlack ? BISHOP_ID * -1 : BISHOP_ID);
+
+      break;
+    case SPECIAL_MOVE_PROMOTION_KNIGHT:
+      from_piece = (bBlack ? KNIGHT_ID * -1 : KNIGHT_ID);
+
+      break;
+  }
+
+  if (invalid_squares) {
+    invalid_squares[(*num_invalid_squares)++] = gamept->moves[gamept->curr_move].from;
+    invalid_squares[(*num_invalid_squares)++] = gamept->moves[gamept->curr_move].to;
+  }
+
+  set_piece1(board,gamept->moves[gamept->curr_move].to,from_piece);
+
+  set_piece1(board,gamept->moves[gamept->curr_move].from,0);  /* vacate previous square */
+
+  bKingsideCastle = (gamept->moves[gamept->curr_move].special_move_info == SPECIAL_MOVE_KINGSIDE_CASTLE);
+  bQueensideCastle = (gamept->moves[gamept->curr_move].special_move_info == SPECIAL_MOVE_QUEENSIDE_CASTLE);
+
+  if (bKingsideCastle) {
+    if (!(gamept->curr_move % 2)) {
+      // it's White's move
+      set_piece1(board,5,
+        get_piece1(board,7));
+      set_piece1(board,7,0);
+
+      if (invalid_squares) {
+        invalid_squares[(*num_invalid_squares)++] = 5;
+        invalid_squares[(*num_invalid_squares)++] = 7;
+      }
+    }
+    else {
+      // it's Blacks's move
+      set_piece1(board,61,
+        get_piece1(board,63));
+      set_piece1(board,63,0);
+
+      if (invalid_squares) {
+        invalid_squares[(*num_invalid_squares)++] = 61;
+        invalid_squares[(*num_invalid_squares)++] = 63;
+      }
+    }
+  }
+  else if (bQueensideCastle) {
+    if (!(gamept->curr_move % 2)) {
+      // it's White's move
+      set_piece1(board,3,
+        get_piece1(board,0));
+      set_piece1(board,0,0);
+
+      if (invalid_squares) {
+        invalid_squares[(*num_invalid_squares)++] = 3;
+        invalid_squares[(*num_invalid_squares)++] = 0;
+      }
+    }
+    else {
+      // it's Blacks's move
+      set_piece1(board,59,
+        get_piece1(board,56));
+      set_piece1(board,56,0);
+
+      if (invalid_squares) {
+        invalid_squares[(*num_invalid_squares)++] = 59;
+        invalid_squares[(*num_invalid_squares)++] = 56;
+      }
+    }
+  }
+}
+
+int get_piece1(unsigned char *board,int board_offset)
+{
+  unsigned int bit_offset;
+  unsigned short piece;
+  int piece_int;
+
+  bit_offset = board_offset * BITS_PER_BOARD_SQUARE;
+
+  piece = get_bits(BITS_PER_BOARD_SQUARE,board,bit_offset);
+  piece_int = piece;
+
+  if (piece & 0x8)
+    piece_int |= 0xfffffff0;
+
+  return piece_int;
+}
+
+int get_piece2(unsigned char *board,int row,int column)
+{
+  int board_offset;
+
+  board_offset = row * 8 + column;
+  return get_piece1(board,board_offset);
+}
+
+void set_piece1(unsigned char *board,int board_offset,int piece)
+{
+  unsigned int bit_offset;
+
+  bit_offset = board_offset * BITS_PER_BOARD_SQUARE;
+  set_bits(BITS_PER_BOARD_SQUARE,board,bit_offset,piece);
+}
+
+void set_piece2(unsigned char *board,int row,int column,int piece)
+{
+  int board_offset;
+
+  board_offset = row * 8 + column;
+  set_piece1(board,board_offset,piece);
+}
+
 void copy_game(struct game *gamept_to,struct game *gamept_from)
 {
   memcpy(gamept_to,gamept_from,sizeof (struct game));
@@ -229,6 +575,126 @@ void GetLine(FILE *fptr,char *line,int *line_len,int maxllen)
 
   line[local_line_len] = 0;
   *line_len = local_line_len;
+}
+
+static int fen2pos(char *line,int line_len,unsigned char *pos_buf,int *black_to_play)
+{
+  int m;
+  int n;
+  int p;
+  int forward_slash_count;
+  int rank_count;
+  int chara;
+  int contig_square_no_piece_count;
+  int piece;
+
+  m = 0;
+
+  for (n = 0; n < CHARS_IN_BOARD; n++)
+    pos_buf[n] = 0;
+
+  forward_slash_count = 0;
+  rank_count = 0;
+
+  *black_to_play = -1;
+
+  for (n = 0; n < line_len; n++) {
+    chara = line[n];
+
+    if (chara == ' ') {
+      forward_slash_count++;
+
+      continue;
+    }
+
+    if (forward_slash_count == 8) {
+      if (chara == 'w') {
+        *black_to_play = 0;
+        break;
+      }
+      if (chara == 'b') {
+        *black_to_play = 1;
+        break;
+      }
+    }
+
+    if (chara == '/') {
+      forward_slash_count++;
+
+      if (rank_count != 8)
+        return 1;
+
+      rank_count = 0;
+    }
+    else if ((chara >= '1') && (chara <= '8'))
+      rank_count += chara - '1' + 1;
+    else {
+      if ((chara == 'P') ||
+          (chara == 'R') ||
+          (chara == 'N') ||
+          (chara == 'B') ||
+          (chara == 'Q') ||
+          (chara == 'K') ||
+          (chara == 'p') ||
+          (chara == 'r') ||
+          (chara == 'n') ||
+          (chara == 'b') ||
+          (chara == 'q') ||
+          (chara == 'k'))
+        rank_count++;
+      else
+        return 2;
+    }
+  }
+
+  if (*black_to_play == -1)
+    return 3;
+
+  p = 0;
+
+  for (m = 0; m < 8; m++) {
+    for (n = 0; n < 8; n++) {
+      chara = line[p++];
+
+      if ((chara >= '1') && (chara <= '8')) {
+        contig_square_no_piece_count = chara - '1' + 1;
+        n += contig_square_no_piece_count - 1;
+      }
+      else {
+        if (chara == 'P')
+          piece = PAWN_ID;
+        else if (chara == 'R')
+          piece = ROOK_ID;
+        else if (chara == 'N')
+          piece = KNIGHT_ID;
+        else if (chara == 'B')
+          piece = BISHOP_ID;
+        else if (chara == 'Q')
+          piece = QUEEN_ID;
+        else if (chara == 'K')
+          piece = KING_ID;
+        else if (chara == 'p')
+          piece = PAWN_ID * -1;
+        else if (chara == 'r')
+          piece = ROOK_ID * -1;
+        else if (chara == 'n')
+          piece = KNIGHT_ID * -1;
+        else if (chara == 'b')
+          piece = BISHOP_ID * -1;
+        else if (chara == 'q')
+          piece = QUEEN_ID * -1;
+        else
+          piece = KING_ID * -1;
+
+        set_bits(4,pos_buf,((7 - m) * 8 * 4) + (n * 4),piece);
+      }
+    }
+
+    if (m < 7)
+      p++;
+  }
+
+  return 0;
 }
 
 #define MAX_LINE_LEN 256
@@ -296,4 +762,201 @@ int populate_board_from_board_file(unsigned char *board,char *filename)
   fclose(fptr);
 
   return 0;
+}
+
+int populate_initial_board_from_board_file(char *filename)
+{
+  return populate_board_from_board_file(initial_board,filename);
+}
+
+int populate_board_from_bin_board_file(unsigned char *board,char *filename)
+{
+  struct stat stat_buf;
+  int fhndl;
+  unsigned int bytes_to_read;
+  unsigned int bytes_read;
+
+  if (stat(filename,&stat_buf) == -1)
+    return 1;
+
+  if (stat_buf.st_size != CHARS_IN_BOARD)
+    return 2;
+
+  if ((fhndl = open(filename,O_RDONLY | O_BINARY)) == -1)
+    return 3;
+
+  bytes_to_read = CHARS_IN_BOARD;
+
+  bytes_read = read(fhndl,(char *)board,bytes_to_read);
+
+  if (bytes_read != bytes_to_read) {
+    close(fhndl);
+    return 4;
+  }
+
+  close(fhndl);
+
+  return 0;
+}
+
+int populate_piece_counts_from_piece_count_file(int *piece_counts,char *filename)
+{
+  struct stat stat_buf;
+  int fhndl;
+  unsigned int bytes_to_read;
+  unsigned int bytes_read;
+
+  if (stat(filename,&stat_buf) == -1)
+    return 1;
+
+  if (stat_buf.st_size != (NUM_PIECE_TYPES_0 * 2) * sizeof (int))
+    return 2;
+
+  if ((fhndl = open(filename,O_RDONLY | O_BINARY)) == -1)
+    return 3;
+
+  bytes_to_read = (NUM_PIECE_TYPES_0 * 2) * sizeof (int);
+
+  bytes_read = read(fhndl,(char *)piece_counts,bytes_to_read);
+
+  if (bytes_read != bytes_to_read) {
+    close(fhndl);
+    return 4;
+  }
+
+  close(fhndl);
+
+  return 0;
+}
+
+int populate_initial_board_from_bin_board_file(char *filename)
+{
+  return populate_board_from_bin_board_file(initial_board,filename);
+}
+
+int write_board_to_binfile(unsigned char *board,char *filename)
+{
+  int fhndl;
+  unsigned int bytes_to_write;
+  unsigned int bytes_written;
+
+  if ((fhndl = open(filename,O_CREAT | O_TRUNC | O_WRONLY | O_BINARY,
+      S_IREAD | S_IWRITE)) == -1)
+    return 1;
+
+  bytes_to_write = CHARS_IN_BOARD;
+
+  bytes_written = write(fhndl,(char *)board,bytes_to_write);
+
+  if (bytes_written != bytes_to_write) {
+    close(fhndl);
+    return 2;
+  }
+
+  close(fhndl);
+
+  return 0;
+}
+
+int count_num_pieces(int color,struct game *gamept)
+{
+  int n;
+  short piece;
+  int count;
+
+  count = 0;
+
+  for (n = 0; n < NUM_BOARD_SQUARES; n++) {
+    piece = get_piece1(gamept->board,n);
+
+    if (!piece)
+      continue;
+
+    if (color == WHITE) {
+      if (piece > 0)
+        count++;
+    }
+    else {
+      if (piece < 0)
+        count++;
+    }
+  }
+
+  return count;
+}
+
+void get_piece_counts(unsigned char *board,int *piece_counts)
+{
+  int n;
+  short piece;
+
+  for (n = 0; n < NUM_PIECE_TYPES_0 * 2; n++) {
+    piece_counts[n] = 0;
+  }
+
+  for (n = 0; n < NUM_BOARD_SQUARES; n++) {
+    piece = get_piece1(board,n);
+
+    if (!piece)
+      continue;
+
+    if (piece > 0) {
+      piece_counts[piece - 1]++;
+    }
+    else {
+      piece *= -1;
+      piece_counts[NUM_PIECE_TYPES_0 + piece - 1]++;
+    }
+  }
+}
+
+int piece_counts_match(int *piece_counts,int *match_piece_counts,bool bExactMatch)
+{
+  int n;
+
+  for (n = 0; n < NUM_PIECE_TYPES_0 * 2; n++) {
+    if (match_piece_counts[n] == -1) {
+      if (piece_counts[n])
+        return 0;
+      else
+        continue;
+    }
+
+    if (bExactMatch) {
+      if (piece_counts[n] != match_piece_counts[n])
+        return 0;
+    }
+    else {
+      if (match_piece_counts[n] && (piece_counts[n] != match_piece_counts[n]))
+        return 0;
+    }
+  }
+
+  return 1;
+}
+
+void print_piece_counts(int *piece_counts)
+{
+  int n;
+  int square;
+
+  putchar(0x0a);
+
+  for (n = 0; n < NUM_PIECE_TYPES_0 * 2; n++) {
+    if (!n)
+      square = 1;
+    else if (n == NUM_PIECE_TYPES_0)
+      square = -1;
+
+    if (n < (NUM_PIECE_TYPES_0 * 2) - 1) {
+      printf("%c %d ",format_square(square),piece_counts[n]);
+
+      if (square > 0)
+        square++;
+      else
+        square--;
+    }
+    else
+      printf("%c %d\n",format_square(square),piece_counts[n]);
+  }
 }
