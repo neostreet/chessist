acae922a35554a54f00fd899d7b2271898fc60a6
neostreet <lloyd.aidan@gmail.com>
2024-05-08 12:20:58 -0400

    synch update_board() logic with gargantua's

--- common/chessdbg.c
+++ common/chessdbg.c
@@ -118,7 +118,7 @@ void print_game(struct game *gamept)
     if (gamept->curr_move % 2)
       putchar(0x0a);
 
-    update_board(gamept->board,gamept,NULL,NULL);
+    update_board(gamept,NULL,NULL);
   }
 
   if (gamept->num_moves % 2)
