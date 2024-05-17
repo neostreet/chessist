9bb350d352d9305fe47bbaaedf4d9ef765022131
neostreet <lloyd.aidan@gmail.com>
2024-05-17 14:28:33 -0400

    rearrange the game structure so the board doesn't have to be stored when writing a binary file

--- common/chessrd.c
+++ common/chessrd.c
@@ -258,7 +258,7 @@ int read_binary_game(char *filename,struct game *gamept)
   if ((fhndl = open(filename,O_RDONLY | O_BINARY)) == -1)
     return 1;
 
-  bytes_to_read = sizeof (struct game) - sizeof gamept->moves;
+  bytes_to_read = sizeof (struct game) - (sizeof gamept->moves + sizeof gamept->board);
 
   bytes_read = read(fhndl,(char *)gamept,bytes_to_read);
 
@@ -278,6 +278,8 @@ int read_binary_game(char *filename,struct game *gamept)
 
   close(fhndl);
 
+  position_game(gamept,gamept->curr_move);
+
   return 0;
 }
 
@@ -291,7 +293,7 @@ int write_binary_game(char *filename,struct game *gamept)
       S_IREAD | S_IWRITE)) == -1)
     return 1;
 
-  bytes_to_write = sizeof (struct game) - sizeof gamept->moves;
+  bytes_to_write = sizeof (struct game) - (sizeof gamept->moves + sizeof gamept->board);
 
   bytes_written = write(fhndl,(char *)gamept,bytes_to_write);
 
