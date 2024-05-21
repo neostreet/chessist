3129291ecb3a0534203daf877a9953c9b273664c
neostreet <lloyd.aidan@gmail.com>
2024-05-20 21:49:36 -0400

    synch up with chess_utilities

--- common/chessmvs.c
+++ common/chessmvs.c
@@ -860,3 +860,143 @@ int king_move2(
 
   return retval;
 }
+
+void legal_pawn_moves(struct game *gamept,char current_board_position,struct move *legal_moves,int *legal_moves_count)
+{
+  int square;
+  int rank;
+  int file;
+
+  printf("legal_pawn_moves()\n"); // for now
+
+  square = get_piece1(gamept->board,current_board_position);
+  rank = RANK_OF(current_board_position);
+  file = FILE_OF(current_board_position);
+  printf("square = %d, rank = %d, file = %d\n",square,rank,file);
+}
+
+void legal_rook_moves(struct game *gamept,char current_board_position,struct move *legal_moves,int *legal_moves_count)
+{
+  int square;
+  int rank;
+  int file;
+
+  printf("legal_rook_moves()\n"); // for now
+
+  square = get_piece1(gamept->board,current_board_position);
+  rank = RANK_OF(current_board_position);
+  file = FILE_OF(current_board_position);
+  printf("square = %d, rank = %d, file = %d\n",square,rank,file);
+}
+
+struct knight_move_offset {
+  char rank_offset;
+  char file_offset;
+};
+
+struct knight_move_offset offsets[] = {
+  1, 2,
+  2, 1,
+  1, -2,
+  2, -1,
+  -1, -2,
+  -2, -1,
+  -1, 2,
+  -2, 1
+};
+#define NUM_OFFSETS (sizeof offsets / sizeof(struct knight_move_offset))
+
+void legal_knight_moves(struct game *gamept,char current_board_position,struct move *legal_moves,int *legal_moves_count)
+{
+  int n;
+  int square;
+  int rank;
+  int file;
+  int work_rank;
+  int work_file;
+  int square2;
+  bool bBlack;
+
+  square = get_piece1(gamept->board,current_board_position);
+  rank = RANK_OF(current_board_position);
+  file = FILE_OF(current_board_position);
+  printf("square = %d, rank = %d, file = %d\n",square,rank,file);
+
+  for (n = 0; n < NUM_OFFSETS; n++) {
+    work_rank = rank + offsets[n].rank_offset;
+    work_file = file + offsets[n].file_offset;
+
+    if ((work_rank < 0) || (work_rank >= NUM_RANKS))
+      continue;
+
+    if ((work_file < 0) || (work_file >= NUM_FILES))
+      continue;
+
+    square2 = get_piece2(gamept->board,work_rank,work_file);
+
+    // can't capture a piece of the same color
+    if ((square * square2) > 0)
+      continue;
+
+    // don't allow moves which would put the mover in check; use a scratch game
+    // to achieve this
+
+    copy_game(&scratch,gamept);
+    scratch.moves[scratch.curr_move].from = current_board_position;
+    scratch.moves[scratch.curr_move].to = POS_OF(work_rank,work_file);
+    update_board(&scratch,NULL,NULL);
+    bBlack = scratch.curr_move & 0x1;
+
+    if (player_is_in_check(bBlack,scratch.board,scratch.curr_move))
+      continue;
+
+    if (*legal_moves_count < MAX_LEGAL_MOVES) {
+      legal_moves[*legal_moves_count].from = current_board_position;
+      legal_moves[*legal_moves_count].to = POS_OF(work_rank,work_file);
+      legal_moves[*legal_moves_count].special_move_info = 0;
+      (*legal_moves_count)++;
+    }
+  }
+}
+
+void legal_bishop_moves(struct game *gamept,char current_board_position,struct move *legal_moves,int *legal_moves_count)
+{
+  int square;
+  int rank;
+  int file;
+
+  printf("legal_bishop_moves()\n"); // for now
+
+  square = get_piece1(gamept->board,current_board_position);
+  rank = RANK_OF(current_board_position);
+  file = FILE_OF(current_board_position);
+  printf("square = %d, rank = %d, file = %d\n",square,rank,file);
+}
+
+void legal_queen_moves(struct game *gamept,char current_board_position,struct move *legal_moves,int *legal_moves_count)
+{
+  int square;
+  int rank;
+  int file;
+
+  printf("legal_queen_moves()\n"); // for now
+
+  square = get_piece1(gamept->board,current_board_position);
+  rank = RANK_OF(current_board_position);
+  file = FILE_OF(current_board_position);
+  printf("square = %d, rank = %d, file = %d\n",square,rank,file);
+}
+
+void legal_king_moves(struct game *gamept,char current_board_position,struct move *legal_moves,int *legal_moves_count)
+{
+  int square;
+  int rank;
+  int file;
+
+  printf("legal_king_moves()\n"); // for now
+
+  square = get_piece1(gamept->board,current_board_position);
+  rank = RANK_OF(current_board_position);
+  file = FILE_OF(current_board_position);
+  printf("square = %d, rank = %d, file = %d\n",square,rank,file);
+}
