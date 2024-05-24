f9e9ba72adbe31dbc9f11ef616147d96e016e821
neostreet <lloyd.aidan@gmail.com>
2024-05-24 16:53:22 -0400

    revert previous commit

--- common/chess.h
+++ common/chess.h
@@ -1,64 +1,123 @@
-#ifdef MAKE_GLOBALS_HERE
-#define GLOBAL /* nothing = real object declaration */
-#else
-#define GLOBAL extern
-#endif
+#define WHITE 0
+#define BLACK 1
+#define NUM_PLAYERS 2
 
-GLOBAL char err_msg[80];
+#define NUM_RANKS 8
+#define NUM_FILES 8
 
-GLOBAL int dbg_move;
-GLOBAL int debug_level;
-GLOBAL FILE *debug_fptr;
+#define NUM_BOARD_SQUARES (NUM_RANKS * NUM_FILES)
 
-GLOBAL int bBig;
+#define CHARS_IN_BOARD \
+(NUM_BOARD_SQUARES / 2)  // 64 squares / 2 (nibbles per char)
 
-GLOBAL int highlight_rank;
-GLOBAL int highlight_file;
+#define PAWN_ID           1
+#define ROOK_ID           2
+#define KNIGHT_ID         3
+#define BISHOP_ID         4
+#define QUEEN_ID          5
+#define KING_ID           6
+#define NUM_PIECE_TYPES_0 6
+#define EMPTY_ID          7
 
-GLOBAL int move_start_square;
-GLOBAL int move_start_square_piece;
-GLOBAL int move_end_square;
-GLOBAL int move_end_square_piece;
+#define NUM_PIECES_PER_PLAYER 16
 
-GLOBAL int do_castle_failures;
-GLOBAL int do_castle_successes;
-GLOBAL int seed;
-GLOBAL bool bBinaryFormat;
+#define FORCE_VALUE_QUEEN  9
+#define FORCE_VALUE_ROOK   5
+#define FORCE_VALUE_KNIGHT 3
+#define FORCE_VALUE_BISHOP 3
+#define FORCE_VALUE_PAWN   1
+#define FORCE_VALUE_KING   0
 
-#ifdef MAKE_GLOBALS_HERE
-char piece_ids[] = "RNBQK";
-char *piece_names[] = {
-  "PAWN",
-  "ROOK",
-  "KNIGHT",
-  "BISHOP",
-  "QUEEN",
-  "KING"
+#define SPECIAL_MOVE_NONE                      0x0000
+#define SPECIAL_MOVE_TWO_SQUARE_PAWN_ADVANCE   0x0001
+#define SPECIAL_MOVE_KINGSIDE_CASTLE           0x0002
+#define SPECIAL_MOVE_QUEENSIDE_CASTLE          0x0004
+#define SPECIAL_MOVE_CHECK                     0x0008
+#define SPECIAL_MOVE_CAPTURE                   0x0010
+#define SPECIAL_MOVE_EN_PASSANT_CAPTURE        0x0020
+#define SPECIAL_MOVE_PROMOTION_QUEEN           0x0040
+#define SPECIAL_MOVE_PROMOTION_ROOK            0x0080
+#define SPECIAL_MOVE_PROMOTION_KNIGHT          0x0100
+#define SPECIAL_MOVE_PROMOTION_BISHOP          0x0200
+#define SPECIAL_MOVE_MATE                      0x0400
+
+#define ORIG_FORCE_VALUE (FORCE_VALUE_QUEEN + 2 * FORCE_VALUE_ROOK + \
+2 * FORCE_VALUE_KNIGHT + 2 * FORCE_VALUE_BISHOP + 8 * FORCE_VALUE_PAWN)
+
+#define WORDLEN 80
+#define MAX_MOVES 400
+#define MAX_LEGAL_MOVES 500
+
+#define WIDTH_IN_PIXELS 50
+#define XLEN WIDTH_IN_PIXELS
+#define PIXELS_PER_BYTE 8
+#define WIDTH_IN_BYTES ((WIDTH_IN_PIXELS + PIXELS_PER_BYTE - 1) / PIXELS_PER_BYTE)
+
+#define HEIGHT_IN_PIXELS 50
+#define YLEN HEIGHT_IN_PIXELS
+
+#define IMAGEBUF_LEN (WIDTH_IN_BYTES * HEIGHT_IN_PIXELS)
+#define PIECE_WIDTH IMAGEBUF_LEN
+
+#define BITS_PER_BYTE 8
+
+#define SHRUNK_WIDTH_IN_PIXELS (WIDTH_IN_PIXELS / 2)
+#define SHRUNK_BITS_PER_PIXEL 2
+#define SHRUNK_WIDTH_IN_BITS (SHRUNK_WIDTH_IN_PIXELS * SHRUNK_BITS_PER_PIXEL)
+#define SHRUNK_WIDTH_IN_BYTES ((SHRUNK_WIDTH_IN_BITS + \
+BITS_PER_BYTE - 1) / BITS_PER_BYTE)
+
+#define SHRUNK_HEIGHT_IN_PIXELS (HEIGHT_IN_PIXELS / 2)
+
+#define SHRUNK_IMAGEBUF_LEN (SHRUNK_WIDTH_IN_BYTES * SHRUNK_HEIGHT_IN_PIXELS)
+
+#define BOARD_WIDTH (NUM_FILES * width_in_pixels)
+#define BOARD_HEIGHT (NUM_RANKS * height_in_pixels)
+
+#define NUM_PIECE_TYPES 5
+
+struct move {
+  char from;
+  char to;
+  int special_move_info;
 };
 
-char fmt_str[] = "%s\n";
-char *special_moves[] = {
-  "SPECIAL_MOVE_TWO_SQUARE_PAWN_ADVANCE",
-  "SPECIAL_MOVE_KINGSIDE_CASTLE",
-  "SPECIAL_MOVE_QUEENSIDE_CASTLE",
-  "SPECIAL_MOVE_CHECK",
-  "SPECIAL_MOVE_CAPTURE",
-  "SPECIAL_MOVE_EN_PASSANT_CAPTURE",
-  "SPECIAL_MOVE_PROMOTION_QUEEN",
-  "SPECIAL_MOVE_PROMOTION_ROOK",
-  "SPECIAL_MOVE_PROMOTION_KNIGHT",
-  "SPECIAL_MOVE_PROMOTION_BISHOP",
-  "SPECIAL_MOVE_MATE"
+struct piece_info {
+  char piece_id;
+  char current_board_position;
+  char move_count;
 };
-int num_special_moves = (sizeof special_moves / sizeof (char *));
-struct move legal_moves[MAX_LEGAL_MOVES];
-int legal_moves_count;
-#else
-extern char piece_ids[];
-extern char *piece_names[];
-extern char fmt_str[];
-extern char *special_moves[];
-extern int num_special_moves;
-extern struct move legal_moves[];
-extern int legal_moves_count;
-#endif
+
+#define BITS_PER_BOARD_SQUARE 4
+
+#define FONT_HEIGHT 12
+#define FONT_WIDTH 9
+
+#define Y_PIXELS 200
+
+#define MAX_ANNOTATION_LINE_LEN 25
+#define MAX_ANNOTATION_LINES (Y_PIXELS / FONT_HEIGHT)
+
+#define ANNOTATION_X (8 * XLEN + 2 + FONT_WIDTH)
+#define ANNOTATION_Y 5
+
+#define MAX_FILE_NAME_LEN 32
+#define MAX_TITLE_LEN 128
+
+struct game {
+  char title[MAX_TITLE_LEN];
+  int orientation;
+  int num_moves;
+  int curr_move;
+  struct move moves[MAX_MOVES];
+  unsigned char board[CHARS_IN_BOARD];  /* 8 columns * 8 rows / 2 (nibbles per char) */
+  struct piece_info white_pieces[NUM_PIECES_PER_PLAYER];
+  struct piece_info black_pieces[NUM_PIECES_PER_PLAYER];
+};
+
+struct move_offset {
+  char rank_offset;
+  char file_offset;
+};
+
+typedef char (*CHESS_FILE_LIST)[MAX_FILE_NAME_LEN];
