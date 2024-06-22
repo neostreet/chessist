0fe99220820a1ee65d052bf6cfe0ab12850a71bc
neostreet <lloyd.aidan@gmail.com>
2024-06-21 20:15:50 -0400

    add IDM_GO_TO_MOVE_NUMBER and IDC_MOVE_NUMBER

--- common/chessist.h
+++ common/chessist.h
@@ -20,18 +20,20 @@
 #define IDM_PRINT_PIECE_INFO   204
 #define IDM_MAKE_A_MOVE        205
 #define IDM_PLAY_VS_MAKE_A_MOVE        206
+#define IDM_GO_TO_MOVE_NUMBER  207
 
-#define IDM_TOGGLE_ORIENTATION 207
-#define IDM_TOGGLE_BOARD_SIZE  208
+#define IDM_TOGGLE_ORIENTATION 208
+#define IDM_TOGGLE_BOARD_SIZE  209
 
-#define IDM_PREV_MOVE          209
-#define IDM_NEXT_MOVE          210
-#define IDM_START_OF_GAME      211
-#define IDM_END_OF_GAME        212
+#define IDM_PREV_MOVE          210
+#define IDM_NEXT_MOVE          211
+#define IDM_START_OF_GAME      212
+#define IDM_END_OF_GAME        213
 
-#define IDM_ABOUT              213
+#define IDM_ABOUT              214
 
 #define IDC_STATIC -1
 
 #define DLG_VERFIRST        400
 #define IDC_COMPANY        DLG_VERFIRST
+#define IDC_MOVE_NUMBER    DLG_VERFIRST+1
