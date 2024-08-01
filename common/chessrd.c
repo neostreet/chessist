4dde69b0730dc0050135530815f37e6473cc8172
neostreet <lloyd.aidan@gmail.com>
2024-07-31 22:24:21 -0400

    synch up with chess_utilities

--- common/chessrd.c
+++ common/chessrd.c
@@ -306,14 +306,32 @@ int read_game(char *filename,struct game *gamept,char *err_msg)
 
     bBlack = gamept->curr_move & 0x1;
 
-    if (player_is_in_check(bBlack,gamept->board,gamept->curr_move))
+    if (player_is_in_check(bBlack,gamept->board,gamept->curr_move)) {
       gamept->moves[gamept->curr_move-1].special_move_info |= SPECIAL_MOVE_CHECK;
 
-    if (queen_is_attacked(bBlack,gamept->board,gamept->curr_move))
+      if (debug_fptr) {
+        fprintf(debug_fptr,"read_game: curr_move = %d, set SPECIAL_MOVE_CHECK\n",gamept->curr_move);
+        fprint_bd2(gamept->board,debug_fptr);
+      }
+    }
+
+    if (queen_is_attacked(bBlack,gamept->board,gamept->curr_move)) {
       gamept->moves[gamept->curr_move-1].special_move_info |= SPECIAL_MOVE_QUEEN_IS_ATTACKED;
 
-    if (mate_in_one_exists(gamept))
+      if (debug_fptr) {
+        fprintf(debug_fptr,"read_game: curr_move = %d, set SPECIAL_MOVE_QUEEN_IS_ATTACKED\n",gamept->curr_move);
+        fprint_bd2(gamept->board,debug_fptr);
+      }
+    }
+
+    if (mate_in_one_exists(gamept)) {
       gamept->moves[gamept->curr_move-1].special_move_info |= SPECIAL_MOVE_MATE_IN_ONE;
+
+      if (debug_fptr) {
+        fprintf(debug_fptr,"read_game: curr_move = %d, set SPECIAL_MOVE_MATE_IN_ONE\n",gamept->curr_move);
+        fprint_bd2(gamept->board,debug_fptr);
+      }
+    }
   }
 
   fclose(fptr);
@@ -323,10 +341,22 @@ int read_game(char *filename,struct game *gamept,char *err_msg)
 
   if (!legal_moves_count) {
     // determine if this is a checkmate or a stalemate
-    if (gamept->moves[gamept->curr_move-1].special_move_info & SPECIAL_MOVE_CHECK)
+    if (gamept->moves[gamept->curr_move-1].special_move_info & SPECIAL_MOVE_CHECK) {
       gamept->moves[gamept->curr_move-1].special_move_info |= SPECIAL_MOVE_MATE;
-    else
+
+      if (debug_fptr) {
+        fprintf(debug_fptr,"read_game: curr_move = %d, set SPECIAL_MOVE_MATE\n",gamept->curr_move);
+        fprint_bd2(gamept->board,debug_fptr);
+      }
+    }
+    else {
       gamept->moves[gamept->curr_move-1].special_move_info |= SPECIAL_MOVE_STALEMATE;
+
+      if (debug_fptr) {
+        fprintf(debug_fptr,"read_game: curr_move = %d, set SPECIAL_MOVE_STALEMATE\n",gamept->curr_move);
+        fprint_bd2(gamept->board,debug_fptr);
+      }
+    }
   }
 
   if (got_error)
