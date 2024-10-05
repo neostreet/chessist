fde5ab4f6eb77c73b49a7694b52701187de2afd8
neostreet <lloyd.aidan@gmail.com>
2024-10-04 22:41:09 -0400

    synch up with gargantua

--- common/chessist.h
+++ common/chessist.h
@@ -17,24 +17,26 @@
 #define IDM_PRINT_BOARD        201
 #define IDM_PRINT_GAME         202
 #define IDM_PRINT_MOVES        203
-#define IDM_PRINT_PIECE_INFO   204
-#define IDM_MAKE_A_MOVE        205
-#define IDM_PLAY_VS_MAKE_A_MOVE        206
-#define IDM_GO_TO_MOVE_NUMBER  207
+#define IDM_GO_TO_MOVE_NUMBER  204
 
-#define IDM_TOGGLE_ORIENTATION   208
-#define IDM_TOGGLE_BOARD_SIZE    209
-#define IDM_TOGGLE_COLOR_CHANGES 210
+#define IDM_TOGGLE_ORIENTATION   205
+#define IDM_TOGGLE_BOARD_SIZE    206
+#define IDM_TOGGLE_COLOR_CHANGES 207
+#define IDM_TOGGLE_AUTO_SAVE     208
+#define IDM_TOGGLE_AUTO_ADVANCE  209
 
-#define IDM_PREV_MOVE          211
-#define IDM_NEXT_MOVE          212
-#define IDM_START_OF_GAME      213
-#define IDM_END_OF_GAME        214
+#define IDM_PREV_MOVE          210
+#define IDM_NEXT_MOVE          211
+#define IDM_START_OF_GAME      212
+#define IDM_END_OF_GAME        213
 
-#define IDM_ABOUT              215
+#define IDM_ABOUT              214
 
-#define IDM_PREV_GAME          216
-#define IDM_NEXT_GAME          217
+#define IDM_PREV_GAME          215
+#define IDM_NEXT_GAME          216
+
+#define IDM_SHOW_PUZZLE_STATS  217
+#define IDM_CLEAR_PUZZLE_STATS 218
 
 #define IDC_STATIC -1
 
