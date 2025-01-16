b8295c1b7eedb9f06b4959a822069d2ade65e17d
neostreet <lloyd.aidan@gmail.com>
2025-01-16 00:29:37 -0500

    synch up with chess_utilities

--- common/chess.h
+++ common/chess.h
@@ -113,6 +113,7 @@ struct piece_info {
 struct game {
   char title[MAX_TITLE_LEN];
   int orientation;
+  int has_custom_initial_board;
   int black_moves_first;
   int num_moves;
   int curr_move;
