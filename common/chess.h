c38390d1004318714209954ba258d21577834a1b
neostreet <lloyd.aidan@gmail.com>
2025-03-23 23:54:47 -0400

    synch up with chess_utilities

--- common/chess.h
+++ common/chess.h
@@ -44,10 +44,6 @@
 #define SPECIAL_MOVE_QUEEN_IS_ATTACKED         0x1000
 #define SPECIAL_MOVE_MATE_IN_ONE               0x2000
 
-#define WHITE_WIN 1
-#define BLACK_WIN 2
-#define DRAW      3
-
 #define RESULT_EMPTY 0
 #define RESULT_WIN   1
 #define RESULT_DRAW  2
